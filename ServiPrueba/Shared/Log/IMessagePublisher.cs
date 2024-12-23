579cf982d5c7f85386306660dccfc9ba6f5dc8b9
tchami007 <cesarromano2007@gmail.com>
2024-12-23 17:55:49 -0300

    Agregado de Objetos omitidos en gitignore

---
+++ ServiPrueba/Shared/Log/IMessagePublisher.cs
@@ -0,0 +1,10 @@
+﻿using ServiPrueba.Application.Models;
+
+namespace ServiPrueba.Shared.Log
+{
+    public interface IMessagePublisher
+    {
+        Task PublishLogAsync(LogEntry logEntry);
+        Task PublishLogAsync(string level, string message, string additionalData);
+    }
+}
---
+++ ServiPrueba/Shared/Log/MessagePublisher.cs
@@ -0,0 +1,36 @@
+﻿using ServiPrueba.Application.Models;
+using ServiPrueba.Infraestructure.Events;
+
+namespace ServiPrueba.Shared.Log
+{
+    public class MessagePublisher : IMessagePublisher
+    {
+        private readonly RabbitMQProducer _producer;
+
+        public MessagePublisher(RabbitMQProducer producer)
+        {
+            _producer = producer;
+        }
+
+        public Task PublishLogAsync(LogEntry logEntry)
+        {
+            _producer.Publish(logEntry);
+            return Task.CompletedTask;
+        }
+
+        public Task PublishLogAsync(string level, string message, string additionalData)
+        {
+            LogEntry nuevo = new LogEntry
+            {
+                Level = level,
+                Message = message,
+                AdditionalData = additionalData,
+                Guid = Guid.NewGuid(),
+                Timestamp = DateTime.UtcNow,
+                ApplicationName = "ServiPrueba"
+            };
+
+            return PublishLogAsync(nuevo);  
+        }
+    }
+}
